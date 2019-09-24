7db6f952e05d56c5b9ec842d92b0697e34756750
Pseudomoaner <48593071+Pseudomoaner@users.noreply.github.com>
2019-09-24 17:05:33 +0100

    Add files via upload

--- Matlab Scripts/PlotEdgeCoordsWithExponentialToLinearModel.m
+++ Matlab Scripts/PlotEdgeCoordsWithExponentialToLinearModel.m
@@ -8,12 +8,13 @@ close all
 
 set(0,'defaulttextinterpreter','latex')
 
-Root = 'C:\Users\olijm\Desktop\cellOnEdgeTest\'; %Root directory where your images are stored
+Root = 'E:\Twork\Colony Expansion Assays\16_05_17_WT_WT_PilK_FlgK_ColonyExpansions\WT(Kolter)\'; %Root directory where your images are stored
 
 rootStem = 'BF_Scene%i'; %Format of the folder name where your brightfield images are stored
 imStem = '\\Frame_%i.tif'; %Format of the file name of each frame of your image sequence
-imNos = 1:20; %Indices of the fields of view associated with this dataset. Note the order of input matters - the field of view to the right of this array will be the bottom most image in the stitching
-reprocess = false; %Whether to reprocess the data from scratch, or load pre-processed data.
+imNos = 24:-1:20; %Indices of the fields of view associated with this dataset. Note the order of input matters - the field of view to the right of this array will be the bottom most image in the stitching
+reprocess = true; %Whether to reprocess the data from scratch, or load pre-processed data.
+stitchStem = 'stitchedSegmentations';
 
 halfPaths = cell(length(imNos),1);
 
@@ -21,24 +22,39 @@ for i = 1:length(imNos)
     halfPaths{i} = [Root,sprintf(rootStem,imNos(i))];
 end
 
+stitchPath = [Root,stitchStem];
+if ~exist(stitchPath,'dir')
+    mkdir(stitchPath)
+end
+
 saveTracePath = [Root,'ExtractedProfiles.mat'];
 
 if ~exist(saveTracePath,'file') || reprocess
-    frameNos = 1:70; %Frames that should be included in the analysis (can cut out frames in which, e.g., the colony edge has expended out of frame)
+    frameNos = 1:280; %Frames that should be included in the analysis (can cut out frames in which, e.g., the colony edge has expended out of frame)
     
-    pxSize = 0.1024000; %In microns per pixel
-    dt = 10; %In minutes per frame
+    pxSize = 0.3225000; %In microns per pixel
+    dt = 2; %In minutes per frame
     
-    imageThresh = 220; %Global threshold to binarise image
-    colonyThresh = 0.1; %Proportion of the binary image (within a strip) that must be white (on average) for it to be classified as part of the colony
-    confluenceFrame = 34; %Frame at which the colony becomes confluent (packing fraction of the whole colony behind the edge becomes greater than colonyThresh). If unsure, make larger than the maximum frame index.
-    sampleWindow = 800; %Maximum distance forwards and backwards algorithm should look relative to previous position to find the next position of the colony edge.
-    imAngle = 0; %Amount(in degrees) that the image should be rotated (counterclockwise) so the colony rises from the bottom of the frame.
-    imOverlap = 7; %Percent overlap between frames
-    noBins = 10; %Number of strips that each image should be split up into.
-    verbose = 1; %Amount of visualisation that should take place. 0 = none, 1 = show edge. Usually best to leave set to 1 to manually verify edge tracking.
+    segSets.segment = true; %Whether to perform segmentation on the stitched images
+    segSets.covThresh = 6; %Threshold local coefficient of variation to detect object - 2 for new Epi (63x w. 2x2 binning), 1.2 for old Epi (63x),
+    segSets.nHood = 9; %Size of local neighbourhood over which COV should be calculated (in pixels) - 15 for 63x images
+    segSets.ridgeScale = 15; %Scale of ridges to be detected - 18 for 63x images, 15 for 20x images
+    segSets.ridgeThresh = 0.08; %Ridge detection threshold
+    segSets.areaThresh = 15; %Minimum area (in pixels) an object needs to be to be included in segmentation. 50 for 63x images
     
-    [edgeYs,edgeXs,Times] = GetColonyEdgeCoords(halfPaths,imStem,frameNos,pxSize,dt,imageThresh,colonyThresh,sampleWindow,confluenceFrame,imAngle,imOverlap,noBins,verbose);
+    stitchSets.imAngle = 90; %Amount(in degrees) that the image should be rotated (counterclockwise) so the colony rises from the bottom of the frame.
+    stitchSets.imOverlap = 7; %Percent overlap between frames
+    stitchSets.normalise = false; %Whether each frame in the stitched tile should be scaled to cover the same range of pixel values.
+    
+    edgeSets.colonyThresh = 0.02; %Proportion of the binary image (within a strip) that must be white (on average) for it to be classified as part of the colony - 0.02 for new Epi (63x w. 2x2 binning), 0.2 for old Epi (63x),
+    edgeSets.confluenceFrame = 450; %Frame at which the colony becomes confluent (packing fraction of the whole colony behind the edge becomes greater than colonyThresh). If unsure, make larger than the maximum frame index.
+    edgeSets.sampleWindow = 500; %Maximum distance forwards and backwards algorithm should look relative to previous position to find the next position of the colony edge.
+    edgeSets.smoothWindow = 50; %The span of the smoothing function used to clean up the packing fraction profile for each strip.
+    edgeSets.noBins = 20; %Number of strips that each image should be split up into.
+    edgeSets.verbose = 1; %Amount of visualisation that should take place. 0 = none, 1 = show edge. Usually best to leave set to 1 to manually verify edge tracking.
+    edgeSets.edgeOffset = 2; %Number of um the actual colony edge is behind the maximum of the dark strips - 4 for new Epi (63x w. 2x2 binning), 2 for old Epi (63x),
+    
+    [edgeYs,edgeXs,Times] = GetColonyEdgeCoords(halfPaths,imStem,stitchPath,frameNos,pxSize,dt,stitchSets,edgeSets,segSets);
     save(saveTracePath)
 else
     load(saveTracePath)
@@ -92,10 +108,10 @@ modelDiffLinY =  f.d * ones(size(Times(31:end-30)));
 dxdt = diff(smooth(medianLocs,80))/dt;
 
 figure(3)
-plot(Times(31:end-30),dxdt(30:end-30),'LineWidth',2)
+plot(Times(11:end-10),dxdt(10:end-10),'LineWidth',2)
 hold on
-plot(Times(31:end-30),modelDiffExpY,'r--','LineWidth',2)
-plot(Times(31:end-30),modelDiffLinY,'k--','LineWidth',2)
+plot(Times(11:end-10),modelDiffExpY,'r--','LineWidth',2)
+plot(Times(11:end-10),modelDiffLinY,'k--','LineWidth',2)
 
 ylim([0,18.0])
 ylabel('\textsf{Colony Expansion Rate ($\mu m$/min)}','FontSize',15)
--- Matlab Scripts/PlotEdgeCoordsWithLinearToLinearModels.m
+++ Matlab Scripts/PlotEdgeCoordsWithLinearToLinearModels.m
@@ -7,12 +7,13 @@ close all
 
 set(0,'defaulttextinterpreter','latex')
 
-Root = 'C:\Users\olijm\Desktop\cellOnEdgeTest\'; %Root directory where your images are stored
+Root = 'E:\Twork\Colony Expansion Assays\16_05_17_WT_WT_PilK_FlgK_ColonyExpansions\WT(Kolter)\'; %Root directory where your images are stored
 
 rootStem = 'BF_Scene%i'; %Format of the folder name where your brightfield images are stored
 imStem = '\\Frame_%i.tif'; %Format of the file name of each frame of your image sequence
-imNos = 1:20; %Indices of the fields of view associated with this dataset. Note the order of input matters - the field of view to the right of this array will be the bottom most image in the stitching
-reprocess = false; %Whether to reprocess the data from scratch, or load pre-processed data.
+imNos = 24:-1:20; %Indices of the fields of view associated with this dataset. Note the order of input matters - the field of view to the right of this array will be the bottom most image in the stitching
+reprocess = true; %Whether to reprocess the data from scratch, or load pre-processed data.
+stitchStem = 'stitchedSegmentations';
 
 halfPaths = cell(length(imNos),1);
 
@@ -20,24 +21,39 @@ for i = 1:length(imNos)
     halfPaths{i} = [Root,sprintf(rootStem,imNos(i))];
 end
 
+stitchPath = [Root,stitchStem];
+if ~exist(stitchPath,'dir')
+    mkdir(stitchPath)
+end
+
 saveTracePath = [Root,'ExtractedProfiles.mat'];
 
 if ~exist(saveTracePath,'file') || reprocess
-    frameNos = 1:285; %Frames that should be included in the analysis (can cut out frames in which, e.g., the colony edge has expended out of frame)
+    frameNos = 1:280; %Frames that should be included in the analysis (can cut out frames in which, e.g., the colony edge has expended out of frame)
     
     pxSize = 0.3225000; %In microns per pixel
-    dt = 1; %In minutes per frame
+    dt = 2; %In minutes per frame
     
-    imageThresh = 160; %Global threshold to binarise image
-    colonyThresh = 0.02; %Proportion of the binary image (within a strip) that must be white (on average) for it to be classified as part of the colony
-    confluenceFrame = 285; %Frame at which the colony becomes confluent (packing fraction of the whole colony behind the edge becomes greater than colonyThresh). If unsure, make larger than the maximum frame index.
-    sampleWindow = 1200; %Maximum distance forwards and backwards algorithm should look relative to previous position to find the next position of the colony edge.
-    imAngle = 90; %Amount(in degrees) that the image should be rotated (counterclockwise) so the colony rises from the bottom of the frame.
-    imOverlap = 7; %Percent overlap between frames
-    noBins = 10; %Number of strips that each image should be split up into.
-    verbose = 1; %Amount of visualisation that should take place. 0 = none, 1 = show edge. Usually best to leave set to 1 to manually verify edge tracking.
+    segSets.segment = true; %Whether to perform segmentation on the stitched images
+    segSets.covThresh = 6; %Threshold local coefficient of variation to detect object - 2 for new Epi (63x w. 2x2 binning), 1.2 for old Epi (63x),
+    segSets.nHood = 9; %Size of local neighbourhood over which COV should be calculated (in pixels) - 15 for 63x images
+    segSets.ridgeScale = 15; %Scale of ridges to be detected - 18 for 63x images, 15 for 20x images
+    segSets.ridgeThresh = 0.08; %Ridge detection threshold
+    segSets.areaThresh = 15; %Minimum area (in pixels) an object needs to be to be included in segmentation. 50 for 63x images
     
-    [edgeYs,edgeXs,Times] = GetColonyEdgeCoords(halfPaths,imStem,frameNos,pxSize,dt,imageThresh,colonyThresh,sampleWindow,confluenceFrame,imAngle,imOverlap,noBins,verbose);
+    stitchSets.imAngle = 90; %Amount(in degrees) that the image should be rotated (counterclockwise) so the colony rises from the bottom of the frame.
+    stitchSets.imOverlap = 7; %Percent overlap between frames
+    stitchSets.normalise = false; %Whether each frame in the stitched tile should be scaled to cover the same range of pixel values.
+    
+    edgeSets.colonyThresh = 0.02; %Proportion of the binary image (within a strip) that must be white (on average) for it to be classified as part of the colony - 0.02 for new Epi (63x w. 2x2 binning), 0.2 for old Epi (63x),
+    edgeSets.confluenceFrame = 450; %Frame at which the colony becomes confluent (packing fraction of the whole colony behind the edge becomes greater than colonyThresh). If unsure, make larger than the maximum frame index.
+    edgeSets.sampleWindow = 500; %Maximum distance forwards and backwards algorithm should look relative to previous position to find the next position of the colony edge.
+    edgeSets.smoothWindow = 50; %The span of the smoothing function used to clean up the packing fraction profile for each strip.
+    edgeSets.noBins = 20; %Number of strips that each image should be split up into.
+    edgeSets.verbose = 1; %Amount of visualisation that should take place. 0 = none, 1 = show edge. Usually best to leave set to 1 to manually verify edge tracking.
+    edgeSets.edgeOffset = 2; %Number of um the actual colony edge is behind the maximum of the dark strips - 4 for new Epi (63x w. 2x2 binning), 2 for old Epi (63x),
+    
+    [edgeYs,edgeXs,Times] = GetColonyEdgeCoords(halfPaths,imStem,stitchPath,frameNos,pxSize,dt,stitchSets,edgeSets,segSets);
     save(saveTracePath)
 else
     load(saveTracePath)
@@ -51,7 +67,7 @@ medianLocs = median(edgeYs,1);
 medianLocs = medianLocs - medianLocs(1);
 
 ft = fittype(@(b,d,xTrans,x) linearToLinear(x,b,d,xTrans));
-f = fit(Times',medianLocs',ft,'StartPoint',[0.03,2,confluenceFrame * dt],'Lower',[0,0,frameNos(1) * dt],'Upper',[1,Inf,frameNos(end) * dt]);
+f = fit(Times',medianLocs',ft,'StartPoint',[0.03,2,edgeSets.confluenceFrame * dt],'Lower',[0,0,frameNos(1) * dt],'Upper',[1,Inf,frameNos(end) * dt]);
 
 initX = Times(1:round(f.xTrans/dt));
 lateX = Times(round(f.xTrans/dt)+1:noFrames);
