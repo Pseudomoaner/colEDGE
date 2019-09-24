a2f5989e0678ba66d4c69095eeb1164ef4e00a7b
Pseudomoaner <48593071+Pseudomoaner@users.noreply.github.com>
2019-09-24 17:10:39 +0100

    Add files via upload

--- Matlab Scripts/stitchFrame.m
+++ Matlab Scripts/stitchFrame.m
@@ -1,4 +1,23 @@
 function frame = stitchFrame(ImRoots,ImStem,frameIndex,stitchSets,segSets)
+%STITCHFRAME stitches together the tiled image in the given directory.
+%   
+%   INPUTS:
+%       -ImRoots: The path to each directory containing each image series.
+%       Should be a Nx1 cell array of strings, where N is the total number
+%       number of fields of view in the tile.
+%       -ImStem: The format of the filename each image is saved as
+%       -frameIndex: The current timepoint you wish to stitch the image
+%       together for
+%       -stitchSets: The settings to define the geometry of the image
+%       stitching
+%       -segSets: The settings used to define the segmentation of the
+%       colony images.
+%
+%   OUTPUTS:
+%       -frame: The stitched image.
+%
+%   Author: Oliver J. Meacock, (c) 2019
+
 frame = [];
 for j = 1:length(ImRoots)
     framePath = [ImRoots{j},sprintf(ImStem,frameIndex)];
